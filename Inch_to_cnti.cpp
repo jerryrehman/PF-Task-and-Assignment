42188aad0be07ea1246c1a06d55f53f3aac25b76
Jerry Rehman <jerryrehman028@gmail.com>
2023-03-08 00:13:37 +0500

    updated

--- Inch_to_cnti.cpp
+++ Inch_to_cnti.cpp
@@ -3,55 +3,30 @@ using namespace std;
 
 int main()
 {
-    int choice;
     int feet, inches;
     float value, in;
     float Total_inches;
 
-    cout<<"\n1) feet without decimal"<<endl;
-    cout<<"2) feet with decimal"<<endl;
-    cin>>choice;
-
-    switch (choice)
+   cout<<"\nEnter the length in feets or feet.inches"<<endl;
+    cin>>value; // for example: value = 34.5
+    feet = value; // because feet is an integer then feet = 34
+    in = value - feet; // in = 0.5
+    if (in >= 0.1 && in <= 0.9) // true
         {
-        case 1:
-
-            cout<<"Enter the length in feet"<<endl;
-            cin>>feet;
-            Total_inches = feet * 12;
-            cout<<"Total inches are "<<Total_inches<<endl;  
-            break;
-        
-        case 2:
-    
-            cout<<"Enter the length in feet.inches"<<endl;
-            cin>>value; // for example: value = 34.5
-            feet = value; // because feet is an integer then feet = 34
-            in = value - feet; // in = 0.5
-            if (in >= 0.1 && in <= 0.9) // true
-                {
-                    inches = in * 10; // inches = 5
-                }
-            if (in >=0.10 && in <= 0.12) // false
-                {
-                    inches = in * 100;
-                }
-            if (in == 0)
-            {
-                inches = 0;
-            }
-            
-        
-            Total_inches = feet*12 + inches; // 34 * 12 + 5 = 413
-            cout<<"Total inches are "<<Total_inches<<endl;  
-            break;
-        
-        default:
-            cout<<"Please Enter the option mention above"<<endl;
-            main();
-            break;
+        inches = in * 10; // inches = 5
         }
-    
+    if (in >=0.10 && in <= 0.12) // false
+        {
+        inches = in * 100;
+        }
+    if (in == 0)
+        {
+        inches = 0;
+        }
+        
+    Total_inches = feet*12 + inches; // 34 * 12 + 5 = 413
+    cout<<"Total inches are "<<Total_inches<<endl;  
+             
     float centi = Total_inches * 2.54;
     cout<<"Total centimeters are "<<centi<<endl;
 
---
+++ pattern.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"          ________________________________                  "<<endl;
+    cout<<"         /              ||               \\                 "<<endl;
+    cout<<"        /               ||                \\                "<<endl;
+    cout<<"       /                ||                 \\               "<<endl;
+    cout<<" ___________________________________________________        "<<endl;
+    cout<<" |                                                 |        "<<endl;
+    cout<<" |                    My Car                       |        "<<endl;
+    cout<<" __/----\\_________________________________/----\\__        "<<endl;
+    cout<<"   \\----/                                 \\----/          "<<endl;
+    cout<<"                                                            "<<endl;
+    return 0;
+}
