9a5312fc1932be4c043fd3142e777110f593f0ab
Jerry Rehman <jerryrehman028@gmail.com>
2023-03-08 00:45:40 +0500

    HOTEL MANG PROJECT

---
+++ Hotal_mag_sys.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"                       ==================================                  "<<endl;
+    cout<<"                       ||                              ||                   "<<endl;
+    cout<<"                       ||                              ||                   "<<endl;
+    cout<<"                       ||    _______        _______    ||                   "<<endl;
+    cout<<"=======================||   |___|___|      |___|___|   ||                   "<<endl;
+    cout<<"||                     ||   |___|___|      |___|___|   ||==================="<<endl;
+    cout<<"||                     ||                              ||                 ||"<<endl;
+    cout<<"||                     ||                              ||                 ||"<<endl;
+    cout<<"||                     ||             HOTEL            ||                 ||"<<endl;
+    cout<<"||                     ||           MANAGNMENT         ||                 ||"<<endl;
+    cout<<"||       _______       ||             SYSTEM           ||      _______    ||"<<endl;
+    cout<<"||      |___|___|      ||                              ||     |___|___|   ||"<<endl;
+    cout<<"||      |___|___|      ||                              ||     |___|___|   ||"<<endl;
+    cout<<"||                     ||         ______________       ||                 ||"<<endl;
+    cout<<"||                     ||         |     |      |       ||                 ||"<<endl;
+    cout<<"||                     ||         |     |      |       ||                 ||"<<endl;
+    cout<<"||                     ||         |    *|*     |       ||                 ||"<<endl;
+    cout<<"||                     ||         |     |      |       ||                 ||"<<endl;
+    cout<<"||_____________________||______________________________||_________________||"<<endl;
+    cout<<""<<endl;
+    cout<<"############################################################################"<<endl;
+    cout<<"Services offer in our Hotel"<<endl;
+    cout<<"1- Room"<<endl;
+    cout<<"2- Food"<<endl;
+    cout<<"3- Drink"<<endl;
+    cout<<"4- Medicine"<<endl;
+    cout<<"Please Enter the option mention above."<<endl;
+    cout<<"############################################################################"<<endl;
+
+
+
+    return 0;
+}
